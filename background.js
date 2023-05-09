683a67ca2bcc9911cdcb3142bba542d96ee3667f
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-05-09 23:09:15 +0200

    chore: cleanup

--- background.js
+++ background.js
@@ -1,34 +1,31 @@
 /* global browser */
 
-const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
 function notify(title, message = "", iconUrl = "icon.png") {
-    return browser.notifications.create(""+Date.now(),
-        {
-           "type": "basic"
-            ,iconUrl
-            ,title
-            ,message
-        }
-    );
+  return browser.notifications.create("" + Date.now(), {
+    type: "basic",
+    iconUrl,
+    title,
+    message,
+  });
 }
 
 browser.menus.create({
-  title: 'Direct Save',
-  contexts: ['link','image'],
-  onclick: (info,tab) => {
-      const url = info.srcUrl || info.linkUrl;
-      if (typeof url !== 'string' || !/^https?:\/\//.test(url) ) {
-        notify(extname, 'No valid URL');
-        return;
-      }
+  title: "Quick Save",
+  contexts: ["link", "image", "video"],
+  onclick: (info) => {
+    const url = info.srcUrl || info.linkUrl;
+    if (typeof url !== "string" || !/^https?:\/\//.test(url)) {
+      notify(extname, "No valid URL");
+      return;
+    }
 
-      //https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/downloads/download
-      browser.downloads.download({
-        url,saveAs: false
-      });
-  }
+    //https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/downloads/download
+    browser.downloads.download({
+      url,
+      saveAs: false,
+    });
+  },
 });
-
